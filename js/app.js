a69d93b2884417354115114260caa2896a4a98e0
Jim Allen <iggalot.iggy@gmail.com>
2020-09-01 11:36:42 -0500

    Minor background color tweaks.
    Minor timing changes on navbar.

--- js/app.js
+++ js/app.js
@@ -7,12 +7,12 @@ function init() {
     const slides = document.querySelectorAll(".slide");
     const pages = document.querySelectorAll(".page");
     const backgrounds = [
-        `radial-gradient(#2B3760, #0B1023)`,
-        `radial-gradient(#4E3022, #161616)`,
-        `radial-gradient(#4E4342, #161616)`,
-        `radial-gradient(#2B3760, #0B1023)`,
-        `radial-gradient(#4E3022, #161616)`,
-        `radial-gradient(#4E4342, #161616)`,
+        `radial-gradient(#4E2768, #0B1023)`,  // blue
+        `radial-gradient(#4E3022, #161616)`,  // brown
+        `radial-gradient(#2B3760, #0B1023)`,  // blue
+        `radial-gradient(#4E3022, #161616)`,  // gray
+        `radial-gradient(#4E4342, #161616)`,  // gray
+        `radial-gradient(#4E4342, #161616)`,  // gray
 
     ];
     // Tracker
